570afe522b6b66925942a28ba4c1d5f27868d818
tecfu <help@tecfu.com>
2015-07-06 06:54:43 -0700

    Basic functionality.

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
 # tec-table
+
+A table widget for CLI applications in nodejs.
+
+<b>Why</b>
+
+- Automatic text wrapping
+- Colors
+- Optional callbacks on column values
+
+## Example Usage
+
+```
+var header = [
+	{
+		value : "item",
+		headerColor : "cyan",
+		color: "yellow",
+		width : 30
+	},
+	{
+		value : "price",
+		color : "red", 
+		formatter : function(value){
+			var str = "$" + value.toFixed(2);
+			if(value > 5){
+				str = chalk.underline.green(str);
+			}
+			return str;
+		}
+	},
+	{
+		value : "organic",
+		formatter : function(value){
+			if(value === 'yes'){
+				value = chalk.stripColor(value);
+				value = chalk.green(value);
+			}
+			else{
+				value = chalk.white.bgRed(value);
+			}
+			return value;
+		}
+	}
+];
+
+var rows = [
+	["hamburger",2.50,"no"],
+	["el jefe's special cream sauce",0.10,"yes"],
+	["two tacos, rice and beans topped with cheddar cheese",9.80,"no"],
+	["apple slices",1.00,"yes"],
+	["ham sandwich",1.50,"no"],
+	["macaroni, ham and peruvian mozzarella",3.75,"no"]
+];
+
+var t1 = Table(header,rows,{
+	borderStyle : 1,
+	paddingBottom : 0,
+	headerAlignment : "center",
+	alignment : "center",
+	color : "white"
+});
+
+var str1 = t1.render();
+
+console.log(str1);
+```
+
+## Example Output
+
+![Example](examples/images/example-1.png "Example") 
+
+
+## API Reference 
+<!--API-REF-->
+<a name="Table"></a>
+## Table
+**Kind**: global class  
+
+* [Table](#Table)
+  * [Table(header, rows, options)](#new_Table_new)
+  * [.render()](#Table.render) ⇒ <code>String</code>
+
+<a name="new_Table_new"></a>
+### Table(header, rows, options)
+
+| Param | Type | Description |
+| --- | --- | --- |
+| header | <code>array</code> |  |
+| rows | <code>array</code> |  |
+| options | <code>object</code> | Table options |
+| options.marginTop | <code>number</code> |  |
+| options.marginLeft | <code>number</code> |  |
+| options.maxWidth | <code>number</code> |  |
+| options.callback | <code>function</code> |  |
+| options.headerAlignment | <code>string</code> |  |
+| options.alignment | <code>string</code> |  |
+| options.paddingRight | <code>number</code> |  |
+| options.paddingLeft | <code>number</code> |  |
+| options.paddingBottom | <code>number</code> |  |
+| options.paddingTop | <code>number</code> |  |
+| options.color | <code>string</code> |  |
+| options.headerColor | <code>string</code> |  |
+| options.gutter | <code>number</code> |  |
+| options.borderStyle | <code>number</code> |  |
+
+**Example**  
+```
+var Table = require('tec-table');
+Table(header,rows,options);
+```
+<a name="Table.render"></a>
+### Table.render() ⇒ <code>String</code>
+Renders a table to a string
+
+**Kind**: static method of <code>[Table](#Table)</code>  
+**Example**  
+```
+var str = t1.render(); 
+console.log(str); //outputs table
+```
+
+<!--END-API-REF-->
--- src/main.js
+++ src/main.js
@@ -1,162 +1,391 @@
-/** 
- * Includes
- *
- */
-
-
 var merge = require("merge"),
-		colors = require("colors"),
+		chalk = require("chalk"),
+		stripAnsi = require("strip-ansi"),
 		wordwrap = require("wordwrap");
 
-var _public = {},
-		_private = {};
+
+var cls = function(){
 
 
-/** 
- * Private Variables
- *
- */
+	var _public = this._public = {},
+			_private = this._private = {};
 
 
-_private.defaults = {
-	row : {
-	},
-	cell : {
+	/** 
+	 * Private Variables
+	 *
+	 */
+
+
+	_private.defaults = {
+		marginTop : 1,
+		marginLeft : 2,
 		maxWidth : 20,
 		callback : null,
-		alignment : left,
-		padding: 1
-	}
-};
+		headerAlignment : "center",
+		alignment : "center",
+		paddingRight : 0,
+		paddingLeft : 0,
+		paddingBottom : 0,
+		paddingTop : 0,
+		color : false,
+		headerColor : false,
+		gutter : 1,
+		borderStyle : 1,
+		borderStyles : [
+			[
+				{v: " ", l: " ", j: " ", h: " ", r: " "},
+				{v: " ", l: " ", j: " ", h: " ", r: " "},
+				{v: " ", l: " ", j: " ", h: " ", r: " "}
+			],
+			[
+				{v: "|", l: "┌", j: "┬", h: "─", r: "┐"},
+				{v: "|", l: "├", j: "┼", h: "─", r: "┤"},
+				{v: "|", l: "└", j: "┴", h: "─", r: "┘"}
+			]
+		]
+	};
 
-_private.table = {
-	columWidths : [],
-	header : [],
-	body : []
-};
+	_private.table = {
+		columns : [],
+		columnWidths : [],
+		columnInnerWidths : [],
+		header : [],
+		body : []
+	};
 
 
-/**
- * Private Methods
- *
- */
+	/**
+	 * Private Methods
+	 *
+	 */
 
 
-_private.buildRow = function(row,options){
-	return row.map(function(cell,index){
-		return _private.buildCell(cell,index);
-	});
-};
-
-_private.buildCell = function(cell,columnIndex){
-	//Pull column options	
-	var output,
-			columnOptions = _private.table.header[columnIndex];
-	
-	if(typeof cell === 'object'){	
-		 columnOptions = merge(columnOptions,cell); 	
-		 output = cell.value;
-	}	
-		 
-	if(typeof columnOptions.formatter === 'function'){
-		output = columnOptions.formatter(output);
-	}
-
-	//Automatic text wrap
-	output = _private.wrapCellContent(output,columnIndex);
-
-	return output;
-};
-
-_private.wrapCellContent = function(string,columnIndex,columnOptions){
-	var width = _private.table.columWidths[columnIndex],
-			wrapWidth = width;
-
-	//Deduct padding from wrapWidth 
-	wrapWidth = wrapWidth - columnOptions.padding;	
-
-	wrap = wordwrap(wrapWidth);
-	string = wrap(string); 
-	
-	//Add padding to ends of string
-	
-
-	if(!columnOptions.formatter){
-	//Apply default styles
-		//padding
+	_private.buildRow = function(row,options){
+		options = options || {};
+		var minRowHeight = 0;
 		
-		//color
-
-		//alignment
-	}
-	return string;
-};
-
-_private.getColumnWidths = function(row){
-	//Widths as prescribed
-	var widths = row.map(function(cell){
-		if(typeof cell === 'object' && typeof cell.width !=='undefined'){
-			return cell.width;
-		}
-		else{
-			return options.cell.maxWidth;
-		}
-	});
-
-	//Check to make sure widths will fit the current display, or resize.
-	var totalWidth = widths.reduce(function(prev,curr){
-		return prev+curr;
-	});
-
-  if(totalWidth > process.stdout.columns){
-		//recalculate proportionately to fit size
-		var prop = process.stdout.columns > totalWidth;
-		prop = prop.toFixed(2)-0.01;
-		widths = widths.map(function(value){
-			return Math.floor(prop*value);
+		//get row as array of cell arrays
+		var cArrs = row.map(function(cell,index){
+			var c = _private.buildCell(cell,index,options);
+			var cellArr = c.cellArr;
+			if(options.header){
+				_private.table.columnInnerWidths.push(c.width);
+			}
+			minRowHeight = (minRowHeight < cellArr.length) ? cellArr.length : minRowHeight;
+			return cellArr;
 		});
-	}
 
-	return widths;
+		//Adjust minRowHeight to reflect vertical row padding
+		minRowHeight = (options.header) ? minRowHeight : minRowHeight + 
+			(_public.options.paddingBottom + _public.options.paddingTop);
+
+		//convert array of cell arrays to array of lines
+		var lines = Array.apply(null,{length:minRowHeight})
+			.map(Function.call,function(){return []});
+
+		cArrs.forEach(function(cellArr,a){
+			var whiteline = Array(_private.table.columnWidths[a]).join('\ ');
+			if(!options.header){
+				//Add whitespace for top padding
+				for(i=0; i<_public.options.paddingTop; i++){
+					cellArr.unshift(whiteline);
+				}
+				
+				//Add whitespace for bottom padding
+				for(i=0; i<_public.options.paddingBottom; i++){
+					cellArr.push(whiteline);
+				}
+			}	
+			for(var b=0; b<minRowHeight; b++){	
+				lines[b].push((typeof cellArr[b] != 'undefined') ? cellArr[b] : whiteline);
+			}
+		});
+
+		return lines;
+	};
+
+	_private.buildCell = function(cell,columnIndex,options){
+
+		//Pull column options	
+		var output;
+		options = options || {};
+		
+		if(options && options.header){
+			cell = merge(true,_public.options,cell);
+			_private.table.columns.push(cell);
+			output = cell.value;
+			columnOptions = cell;
+		}	
+		else{
+			columnOptions = _private.table.columns[columnIndex];
+			if(typeof cell === 'object'){	
+				columnOptions = merge(true,columnOptions,cell);		
+				cell = value;			
+			}	
+		
+			if(typeof columnOptions.formatter === 'function'){
+				output = columnOptions.formatter(cell);
+			}
+			else{
+				output = cell;
+			}
+		}
+		
+		//Automatic text wrap
+		var wrapObj  = _private.wrapCellContent(output,columnIndex,columnOptions,
+										(options && options.header) ? "header" : "body");
+		output = wrapObj.output;
+		
+		//return as array of lines
+		return {
+			cellArr : output.split('\n'),
+			width : wrapObj.width
+		};
+	};
+
+	_private.colorizeAllWords = function(color,str){
+		//Color each word in the cell so that line breaks don't break color 
+		var arr = str.replace(/(\S+)/gi,function(match){
+			return chalk[color](match)+'\ ';
+		});
+		return arr;
+	};
+
+	_private.colorizeLine = function(color,str){
+		return chalk[color](str);
+	};
+
+	_private.wrapCellContent = function(value,columnIndex,columnOptions,rowType){
+		var string = value.toString(),
+				width = _private.table.columnWidths[columnIndex],
+				innerWidth = width - columnOptions.paddingLeft 
+										- columnOptions.paddingRight
+										- 1; //border/gutter
+
+		//Break string into array of lines
+		wrap = wordwrap(innerWidth);
+		string = wrap(string); 
+
+		var strArr = string.split('\n');
+
+		//Format each line
+		strArr = strArr.map(function(line){
+
+			//Apply colors
+			switch(true){
+				case(rowType === 'header'):
+					line = (columnOptions.color || _public.options.color) ? 
+						_private.colorizeLine(columnOptions.headerColor || _public.options.color,line) : 
+						line;
+					break;
+				case(typeof columnOptions.color === 'string'):
+					line = _private.colorizeLine(columnOptions.color,line);
+					break;
+				case(typeof _public.options.color === 'string'):
+					line = _private.colorizeLine(_public.options.color,line);
+					break;
+				default:
+			}
+			
+			//Left, Right Padding
+			line = Array(columnOptions.paddingLeft).join(' ') +
+						line +
+						Array(columnOptions.paddingRight).join(' ');
+			var lineLength = stripAnsi(line).length;
+
+			//Alignment 
+			var alignTgt = (rowType === 'header') ? "headerAlignment" : "alignment";
+			if(lineLength < width){
+				var spaceAvailable = width - lineLength; 
+				switch(true){
+					case(columnOptions[alignTgt] === 'center'):
+						var even = (spaceAvailable %2 === 0);
+						spaceAvailable = (even) ? spaceAvailable : 
+							spaceAvailable - 1;
+						if(spaceAvailable > 1){
+							line = Array(spaceAvailable/2).join(' ') + 
+								line +
+								Array(spaceAvailable/2 + ((even)?1:2)).join(' ');
+						}
+						break;
+					case(columnOptions[alignTgt] === 'right'):
+						line = Array(spaceAvailable).join(' ') + line;
+						break;
+					default:
+						line = line + Array(spaceAvailable).join(' ');
+				}
+			}
+			
+			return line;
+		});
+
+		string = strArr.join('\n');
+		
+		return {
+			output : string,
+			width : innerWidth
+		};
+	};
+
+	_private.getColumnWidths = function(row){
+		//Widths as prescribed
+		var widths = row.map(function(cell){
+			if(typeof cell === 'object' && typeof cell.width !=='undefined'){
+				return cell.width;
+			}
+			else{
+				return _public.options.maxWidth;
+			}
+		});
+
+		//Check to make sure widths will fit the current display, or resize.
+		var totalWidth = widths.reduce(function(prev,curr){
+			return prev+curr;
+		});
+		//Add marginLeft to totalWidth
+		totalWidth += _public.options.marginLeft;
+
+		if(totalWidth > process.stdout.columns){
+			//recalculate proportionately to fit size
+			var prop = process.stdout.columns > totalWidth;
+			prop = prop.toFixed(2)-0.01;
+			widths = widths.map(function(value){
+				return Math.floor(prop*value);
+			});
+		}
+
+		return widths;
+	};
+
+
+	/** 
+	 * Public Variables
+	 *
+	 */
+
+
+	_public.options = {};
+
+
+	/**
+	 * Public Methods
+	 *
+	 */
+
+
+	_private.setup = function(header,body,options){
+		
+		_public.options = merge(true,_private.defaults,options);
+		
+		_private.table.columnWidths = _private.getColumnWidths(header);
+
+		header = [header];
+		_private.table.header = header.map(function(row){
+			return _private.buildRow(row,{
+				header:true
+			});
+		});
+
+		_private.table.body = body.map(function(row){
+			return _private.buildRow(row);
+		});
+
+		return _public;
+	};
+
+
+	/**
+	 * Renders a table to a string
+	 * @returns {String}
+	 * @memberof Table 
+	 * @example 
+	 * ```
+	 * var str = t1.render(); 
+	 * console.log(str); //outputs table
+	 * ```
+	*/
+	_public.render = function(){
+		var str = '',
+				part = ['header','body'],
+				bArr = [],
+				marginLeft = Array(_public.options.marginLeft + 1).join('\ '),
+				bS = _public.options.borderStyles[_public.options.borderStyle],
+				borders = [];
+
+		//Borders
+		for(a=0;a<3;a++){
+			borders.push('');
+			_private.table.columnWidths.forEach(function(w,i,arr){
+				borders[a] += Array(w).join(bS[a].h) + ((i+1 !== arr.length) ? bS[a].j : bS[a].r);
+			});
+			borders[a] = bS[a].l + borders[a];
+			borders[a] = borders[a].split('');
+			borders[a][borders[a].length1] = bS[a].r;
+			borders[a] = borders[a].join('');
+			borders[a] = marginLeft + borders[a] + '\n';
+		}
+		
+		str += borders[0];
+
+		//Rows
+		part.forEach(function(p,i){
+			while(_private.table[p].length){
+				row = _private.table[p].shift();
+			
+				row.forEach(function(line){
+					str = str 
+						+ marginLeft 
+						+ bS[1].v
+						+	line.join(bS[1].v) 
+						+ bS[1].v
+						+ '\n';
+				});
+				
+				//Joining border
+				if(!(i==1 && _private.table[p].length===0)){
+					str += borders[1];
+				}
+			}	
+		});
+		
+		//Bottom border
+		str += borders[2];
+
+		return Array(_public.options.marginTop + 1).join('\n') + str;
+	}	
+
 };
-
-
-/** 
- * Public Variables
- *
- */
-
-
-_public.options = {};
 
 
 /**
- * Public Methods
+ * @class Table
+ * @param {array} header
+ * @param {array} rows
+ * @param {object} options									 - Table options 
+ * @param {number} options.marginTop
+ * @param {number} options.marginLeft
+ * @param {number} options.maxWidth 
+ * @param {function} options.callback
+ * @param {string} options.headerAlignment
+ * @param {string} options.alignment
+ * @param {number} options.paddingRight
+ * @param {number} options.paddingLeft
+ * @param {number} options.paddingBottom 
+ * @param {number} options.paddingTop
+ * @param {string} options.color 
+ * @param {string} options.headerColor 
+ * @param {number} options.gutter 
+ * @param {number} options.borderStyle 
+ * @returns {Table}
+ * @example
+ * ```
+ * var Table = require('tec-table');
+ * Table(header,rows,options);
+ * ```
  *
  */
-
-
-_public.setup = function(header,body,options){
-	
-	_public.options = merge(defaults,options);
-	
-	_private.table.columWidths = _private.getColumnWidths(header);
-
-	_private.table.header = header.map(function(row){
-		return _private.buildRow(row,{
-			header:true
-		});
-	});
-
-	_private.table.body = body.map(function(row){
-		return _private.buildRow(row);
-	});
+module.exports = function(header,rows,options){
+	var o = new cls();
+	return o._private.setup(header,rows,options);
 };
-
-_public.render = function(data){
-
-	var str = '';
-	
-};
-
-module.exports = _public;
