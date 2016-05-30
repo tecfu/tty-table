c8b35cad0cf7103ca3c1bc5b8bafb362a1074456
tecfu <help@tecfu.com>
2016-05-30 11:25:33 -0700

    Alphabetized.

--- src/config.js
+++ src/config.js
@@ -1,20 +1,4 @@
 var Config = {
-	defaultValue : "[32m[37m[41m #ERROR  [49m[32m[39m",
-	marginTop : 1,
-	marginLeft : 2,
-	width : 20,
-	formatter : null,
-	headerAlign : "center",
-	footerAlign : "center",
-	align : "center",
-	paddingRight : 0,
-	paddingLeft : 0,
-	paddingBottom : 0,
-	paddingTop : 0,
-	color : false,
-	headerColor : "yellow",
-	footerColor : false,
-	borderStyle : 1,
 	borderCharacters : [
 		[
 			{v: " ", l: " ", j: " ", h: " ", r: " "},
@@ -32,7 +16,24 @@ var Config = {
 			{v: "|", l: "+", j: "+", h: "-", r: "+"}
 		]
 	],
-	tableType : null //horizontal,vertical,cross
+	align : "center",
+	borderStyle : 1,
+	color : false,
+	compact : false,
+	defaultValue : "[32m[37m[41m #ERROR  [49m[32m[39m",
+	footerAlign : "center",
+	footerColor : false,
+	formatter : null,
+	headerAlign : "center",
+	headerColor : "yellow",
+	marginLeft : 2,
+	marginTop : 1,
+	paddingBottom : 0,
+	paddingLeft : 0,
+	paddingRight : 0,
+	paddingTop : 0,
+	tableType : null,
+	width : 20,
 };
 
 Config.GUTTER = 1;
@@ -40,12 +41,12 @@ Config.GUTTER = 1;
 Config.columnSettings = []; 
 Config.headerEmpty = false;
 Config.table = {
-	columns : [],
-	columnWidths : [],
-	columnInnerWidths : [],
-	header : '', //post-rendered strings.
 	body : '', 
+	columnInnerWidths : [],
+	columnWidths : [],
+	columns : [],
 	footer : '',
+	header : '', //post-rendered strings.
 	height : 0,
 	typeLocked : false //once a table type is selected can't switch
 };
