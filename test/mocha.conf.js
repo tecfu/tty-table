65d38897bcdd5dfe0df0fff612fb99c0295b4f36
tecfu <help@tecfu.com>
2017-06-20 04:38:02 -0700

    Formatting

--- test/mocha.conf.js
+++ test/mocha.conf.js
@@ -6,29 +6,29 @@ var fs = require('fs');
 var glob = require('glob');
 var grunt = require('grunt');
 
-//Get list of all test scripts
+//Test all example scripts against their saved output
 var list = glob.sync('examples/*.js');
 
 list.forEach(function(element,index,array){
+  describe(element,function(){
+    it('Should match ' + element + '-output.txt',function(deferred){
 
-	//Matching test
-	describe(element,function(){
-		it('Should match ' + element + '-output.txt',function(deferred){
+      var exec = require('child_process').exec, child;
 
-			var exec = require('child_process').exec, child;
+      child = exec('node ./'+element+' --color=always',
+        function (error, stdout, stderr) {
+          //console.log('stdout: ' + stdout);
+          //console.log('stderr: ' + stderr);
+          if (error !== null) {
+            grunt.log.error('Exec error: ' + error);
+          }
+          var subname = element.split('.')[0];
+          var expected1 = fs.readFileSync('./'+subname+'-output.txt',{encoding : 'utf-8'});
 
-			child = exec('node ./'+element+' --color=always',
-			function (error, stdout, stderr) {
-				//console.log('stdout: ' + stdout);
-				//console.log('stderr: ' + stderr);
-				if (error !== null) {
-					grunt.log.error('Exec error: ' + error);
-				}
-				var subname = element.split('.')[0];
-				var expected1 = fs.readFileSync('./'+subname+'-output.txt',{encoding : 'utf-8'});
-				stdout.should.equal(expected1);
-				deferred();
-			});
-		});
-	});
+          //example result should match saved output
+          stdout.should.equal(expected1);
+          deferred();
+      });
+    });
+  });
 });
