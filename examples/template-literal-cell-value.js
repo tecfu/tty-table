e1ac3af88bb800b826c2e839e6823f4b62912f31
Tec Fu <help@tecfu.com>
2019-08-28 10:20:15 -0700

    Improved text wrap support for multi-line template literal cell values via smartwrap module update. Closes #41

---
+++ examples/template-literal-cell-value.js
@@ -0,0 +1,19 @@
+var Table = require('../');
+
+const rows = [[`The use of the secure-rm CLI is deprecated.
+Migrate over -> secure-rm-cli. Run:
+npm un secure-rm -g
+npm i secure-rm-cli -g`]]
+
+const t1 = Table([], rows, {
+  borderStyle: 1,
+  borderColor: 'yellow',
+  paddingBottom: 0,
+  headerAlign: 'center',
+  headerColor: 'yellow',
+  align: 'center',
+  color: 'white'
+})
+
+const str1 = t1.render()
+console.log(str1)
