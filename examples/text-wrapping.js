51fb4c8203c6f3dadecb3126db479a51cb08babe
Tec Fu <help@tecfu.com>
2020-05-06 16:26:30 -0700

    chore: Added test for calculating cell length in case of line breaks

--- examples/text-wrapping.js
+++ examples/text-wrapping.js
@@ -2,15 +2,6 @@ const example = require("../test/example-utils.js")
 const Table = require("../")
 const style = Table.style
 
-const rows1 = [
-  [`${style("some red words", "red")}`, `some ${style("bold", "bold")} and ${style("dim", "dim")} words`, "plain test"],
-  ["some plain words", `some ${style("bold", "bold")} and ${style("dim", "dim")} words`, "plain test"]
-]
-const rows2 = [
-  // ["plain text", `a few red words in here`],
-  ["plain text", `a ${style("few red words in", "red")} here`]
-]
-
 function test (testName, tableWidth, rows, config) {
   console.log(`\n${style(testName, "cyan")}`)
   example.init(`Test: ${testName}`, tableWidth)
@@ -21,9 +12,19 @@ function test (testName, tableWidth, rows, config) {
   console.log("")
 }
 
-test("Embedded ansi column width", 50, rows1, {})
+test("Embedded ansi column width", 50, [
+  [`${style("some red words", "red")}`, `some ${style("bold", "bold")} and ${style("dim", "dim")} words`, "plain test"],
+  ["some plain words", `some ${style("bold", "bold")} and ${style("dim", "dim")} words`, "plain test"]
+], {})
 
-test("Embedded ansi breaking", 20, rows2, {})
+test("Embedded ansi breaking", 20, [
+  // ["plain text", `a few red words in here`],
+  ["plain text", `a ${style("few red words in", "red")} here`]
+], {})
+
+test("Handle line breaks", 20, [
+  ["plain text", `a ${style("few red\n words \nin", "red")} \nhere`]
+], {})
 
 test("Embedded emojis", 25, [[
   "aaa bbb ccc",
