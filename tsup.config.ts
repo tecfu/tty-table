b18800b9eb03eb6849684b0a48695715c6c34a45
Tec Fu <help@tecfu.com>
2026-09-05 10:37:51 -0700

    build: add standalone browser bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: {
     index: "src/index.ts",
-    cli: "src/cli.ts"
+    cli: "src/cli.ts",
+    browser: "src/browser.ts"
   },
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
   splitting: false,
-  target: "node20",
-  outDir: "dist"
+  target: "es2020",
+  outDir: "dist",
+  noExternal: ["wcwidth"]
 })
